c46407720dd9408aff66311f11c44b315f85976d
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2023-03-04 13:17:07 +0100

    Task 2 updated

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,21 @@
 import Player from '@vimeo/player';
-const throttle = require('lodash.throttle');
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'videoplayer-current-time';
 
-const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
 const onPlay = function (event) {
-  localStorage.setItem(STORAGE_KEY, event.seconds);
+  try {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(event.seconds));
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
-
 const currentTime = localStorage.getItem(STORAGE_KEY);
 
 player
